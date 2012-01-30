0c668210e719114ff693e874e5500502fc05e56e
algedi <algedi@tin.it>
2012-01-30 23:39:24 +0100

    up

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp "Decrypt" "$HOME/.gnome2/nautilus-scripts/Decrypt"
+cp "Encrypt (ascii)" "$HOME/.gnome2/nautilus-scripts/Encrypt (ascii)"
+cp "Encrypt (bin)" "$HOME/.gnome2/nautilus-scripts/Encrypt (bin)"
+
+chmod
+

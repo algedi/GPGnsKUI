4b6bf983049fbee0bc081a2f1a5181921ccc55b9
algedi <algedi@tin.it>
2012-02-01 23:48:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ cp "Decrypt" "$HOME/.gnome2/nautilus-scripts/Decrypt"
 cp "Encrypt (ascii)" "$HOME/.gnome2/nautilus-scripts/Encrypt (ascii)"
 cp "Encrypt (bin)" "$HOME/.gnome2/nautilus-scripts/Encrypt (bin)"
 
-chmod
-
+chmod a+x "Decrypt"
+chmod a+x "Encrypt (ascii)"
+chmod a+x "Encrypt (bin)"

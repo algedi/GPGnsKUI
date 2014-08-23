ad97e830187c1452f6794bd7051614a5ea5a4d39
algedi <algedi@tin.it>
2014-08-23 21:18:15 +0200

    For new folder nytilus-script

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ cp "Encrypt (bin)" "$HOME/.gnome2/nautilus-scripts/Encrypt (bin)"
 chmod a+x "Decrypt"
 chmod a+x "Encrypt (ascii)"
 chmod a+x "Encrypt (bin)"
+
+#/home/algedi/.local/share/nautilus/scripts/GnuPG
